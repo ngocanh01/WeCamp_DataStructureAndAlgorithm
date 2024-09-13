b7f8b1a6ec923966f5da09653809d97a57be4a75
ngocanh01 <nanh5605@gmail.com>
2024-09-13 10:50:29 +0700

    Valid Anagram

---
+++ WeCamp_DataStructureAndAlgorithm/Problems/ValidAnagram.cs
@@ -0,0 +1,40 @@
+﻿namespace WeCamp_DataStructureAndAlgorithm.Problems
+{
+	public static class ValidAnagram
+	{
+		public static bool Solution(string s, string t)
+		{
+			if (s.Length != t.Length)
+			{
+				return false;
+			}
+			Dictionary<char, int> dic = new Dictionary<char, int>();
+
+			foreach (char i in s)
+			{
+				if (dic.ContainsKey(i))
+				{
+					dic[i] += 1;
+				}
+				else
+				{
+					dic.Add(i, 1);
+				}
+			}
+
+			foreach (char i in t)
+			{
+				if (dic.ContainsKey(i) && dic[i] > 0)
+				{
+					dic[i] -= 1;
+				}
+				else
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+	}
+}
--- WeCamp_DataStructureAndAlgorithm/Program.cs
+++ WeCamp_DataStructureAndAlgorithm/Program.cs
@@ -9,9 +9,12 @@ namespace WeCamp_DataStructureAndAlgorithm
 			/*
 			string s = "abcd", t = "abcde";
 			Console.WriteLine($"Find the different: {FindTheDifference.Solution(s, t)}");
-			*/
+			
 			string s = "ABFCACDB";
-			Console.WriteLine($" Minimum String Length After Removing Substrings: {MinimumStringLengthAfterRemovingSubstrings.Solution(s)}");
+			Console.WriteLine($"Minimum String Length After Removing Substrings: {MinimumStringLengthAfterRemovingSubstrings.Solution(s)}");
+			*/
+			string s = "anagram", t = "nagaram";
+			Console.WriteLine($"Valid Anagram: {ValidAnagram.Solution(s, t)}");
 		}
 	}
 }

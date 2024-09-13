60914d572dcfb465fc024cca95d0ecc9ab568794
ngocanh01 <nanh5605@gmail.com>
2024-09-13 11:08:21 +0700

    Monotonic Array

---
+++ WeCamp_DataStructureAndAlgorithm/Problems/MonotonicArray.cs
@@ -0,0 +1,27 @@
+﻿namespace WeCamp_DataStructureAndAlgorithm.Problems
+{
+	public static class MonotonicArray
+	{
+		public static bool Solution(int[] nums)
+		{
+			bool isIncrease = false, isDecrease = false;
+
+			for (int i = 0; i < nums.Length - 1; i++)
+			{
+				if (nums[i] < nums[i + 1])
+				{
+					isIncrease = true;
+				}
+				if (nums[i] > nums[i + 1])
+				{
+					isDecrease = true;
+				}
+			}
+			if (isIncrease && isDecrease)
+			{
+				return false;
+			}
+			return true;
+		}
+	}
+}
--- WeCamp_DataStructureAndAlgorithm/Program.cs
+++ WeCamp_DataStructureAndAlgorithm/Program.cs
@@ -12,9 +12,12 @@ namespace WeCamp_DataStructureAndAlgorithm
 			
 			string s = "ABFCACDB";
 			Console.WriteLine($"Minimum String Length After Removing Substrings: {MinimumStringLengthAfterRemovingSubstrings.Solution(s)}");
-			*/
+			
 			string s = "anagram", t = "nagaram";
 			Console.WriteLine($"Valid Anagram: {ValidAnagram.Solution(s, t)}");
+			*/
+			int[] nums = { 1, 2, 3, };
+			Console.WriteLine($"Monotonic Array: {MonotonicArray.Solution(nums)}");
 		}
 	}
 }
